42e11e69b75fa4ee3d57b5190adb2f867e205eea
serdukaleksandra@gmail.com <serdukaleksandra1@gmail.com>
2020-11-24 15:48:48 +0200

    Первый комит Саши

--- src/Converter.java
+++ src/Converter.java
@@ -2,6 +2,11 @@ public  class Converter {
     public static double convertInchToCm(double inch){
        // method
         return inch * 2.54;
+
+        /*
+        ЭТО НОВЫЕ СТРОКИ В НОВОМ КОММИТЕ
+         */
+
     }
 }
 
