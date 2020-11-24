ee28610b720fc2fdb621909f4bbf162a5f43ac85
serdukaleksandra@gmail.com <serdukaleksandra1@gmail.com>
2020-11-24 16:42:22 +0200

    Third commit Massiv

---
+++ src/Massiv.java
@@ -0,0 +1,15 @@
+public class Massiv {
+    public static  void main(String[] args){
+        int[] numbers = new int[8];
+        for(int i=0; i<numbers.length; i++){
+            numbers[i]=i*5;
+        }
+        for (int i=0; i<numbers.length; i++){
+            System.out.println(numbers [i]);
+        }
+        int[] sasha={5,10,4,7};
+        for(int i = 0; i < sasha.length; i++){
+            System.out.println(sasha[i]);
+        }
+    }
+}
