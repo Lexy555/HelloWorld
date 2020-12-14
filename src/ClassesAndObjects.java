36a6d8f4e4444c212497987c8fc8d78527b6deb2
serdukaleksandra@gmail.com <serdukaleksandra1@gmail.com>
2020-12-14 15:17:58 +0200

    lesson 18 Setters and Getters, Classes and Objects

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -2,32 +2,39 @@ public class ClassesAndObjects {
 
     public static void main(String[] args) {
         Person person1 = new Person();
-        Person person2 = new Person();
-        person1.setNameAndAge("Sasha" , 26);
-        person2.setNameAndAge("Ваня", 30);
-        person1.speak();
-        person2.speak();
-        person2.calculatedYearsToRetiremnt();
-        int years =person1.calculatedYearsToRetiremnt();
-        System.out.println("До пенсии осталось " +years + " лет");
-
-
+        person1.setName("Какое то имя");
+        person1.setAge(-12);
+        System.out.println("Выводим  значение в main методе: " +person1.getName());
+       System.out.println(" Выводим значение в методе main: "+ person1.getAge());
     }
 }
 class Person {
     private   String name;//ограничиваем доступ к полям
     private  int age;
-    void setNameAndAge(String username, int usersage){
-        name =username;
-        age =usersage;
+   public void setName(String username){
+       if(username.isEmpty()){
+           System.out.println("Ты ввел пустое имя");
+       }else
+        name =username;}
 
+     public String getName(){
+    return name;
+    }
+    public void setAge(int userage) {
+        if (userage < 0) {
+            System.out.println("Возраст должен быть положительным");
+        } else {
+        age = userage;}
+    }
+    public int getAge() {
+        return age;
     }
     int calculatedYearsToRetiremnt(){
         int years  =65 -age;
         return years;
     }
 
-    void speak() {
+    void speak(){
         System.out.println("Я " + name + " мне " + age);
     }
     void sayHello(){
--- src/Hello.java
+++ src/Hello.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args){
+
+        //LetterV.drawV(10);
+
     }
 }
 
